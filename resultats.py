31db00abcdcc39583791d30ef3661f83aa5e37c2
Joscrap <129535698+Joscrap@users.noreply.github.com>
2025-03-07 18:35:50 +0100

    ok

---
+++ resultats.py
@@ -0,0 +1,7 @@
+resultats = {
+
+    "Le Fraggeur Fou 🎯":"Tu es l’alpha duelliste ! Agressif, explosif et toujours en train de chercher des kills. Mais attention à ne pas trop forcer les duels inutiles !",
+    "Le Loup Solitaire 🐺":"Tu joues souvent en solo et fais confiance à ton propre skill. Tu peux carry, mais tu gagnerais à mieux travailler avec ton équipe",
+    "Le Cerveau Stratégique 🧠":"Tu réfléchis avant d’agir et préfères les décisions intelligentes. Un excellent leader qui pourrait devenir un bon IGL (In-Game Leader) !",
+    "Le Contrôleur Tactique 🛡️":"Tu cherches à ralentir le jeu et garder un contrôle total. Patient et intelligent, tu joues pour la victoire plus que pour le show"
+}
